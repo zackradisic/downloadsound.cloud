a7e90aaf6e4d3b0000647d9dc78efbb0e6b6fe2c
Zack Radisic <zack@theradisic.com>
2020-10-29 18:14:08 -0400

    add link to github in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -16,7 +16,7 @@ const Footer = () => {
           {links}
 
           <Columns.Column>
-          © {new Date().getFullYear()} by @zackradisic
+          © {new Date().getFullYear()} by <a href="https://github.com/zackradisic">@zackradisic</a>
           </Columns.Column>
 
         </Columns>
