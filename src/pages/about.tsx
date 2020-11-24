efddfc70d7f6077d75bb616c443f65d3c923e32b
Zack Radisic <zack@theradisic.com>
2020-11-24 13:53:36 -0500

    update color for other pages

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -15,7 +15,7 @@ const About = () => {
     <Layout>
       <SEO></SEO>
 
-      <Section style={{ backgroundColor: '#F8F8F8' }}>
+      <Section style={{ backgroundColor: '#ffe4cc' }}>
         <Container>
           <div style={{ backgroundColor: 'white', padding: '1rem', borderRadius: '5px', boxShadow: '0 1px 2px 0 rgba(0, 0, 0, 0.05)' }}>
             <h1>About</h1>
