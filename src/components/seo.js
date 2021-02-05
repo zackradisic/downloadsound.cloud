06d4646d25c7139074b40989af9f0ac1057d333a
Zack Radisic <zack@theradisic.com>
2021-02-05 04:25:07 +0300

    add tawk.to functionality to site

--- src/components/seo.js
+++ src/components/seo.js
@@ -26,6 +26,24 @@ function SEO ({ description, lang, meta, title }) {
     `
   )
 
+  useEffect(() => {
+    const tawkto = document.createElement('script')
+    tawkto.type = 'text/javascript'
+    tawkto.innerHTML = `
+      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+      (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/601c99b7a9a34e36b973fd84/1etnrgtv3';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+      })();
+    `
+    console.log(tawkto)
+    document.head.appendChild(tawkto)
+  }, [])
+
   const metaDescription = description || site.siteMetadata.description
   const defaultTitle = site.siteMetadata?.title
 
